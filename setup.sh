5aee691589d804d5b364283832f004932be51613
rahairston <rahairston2014@verizon.net>
2023-08-26 10:59:25 -0500

    Fixing setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 USER_ID=$USER
 
-go mod init
+go mod download
 go build -o encrypted-go-backup
 mv encrypted-go-backup install/
 
 sudo install -d -o $USER_ID -g $USER_ID -m 0774 -p /var/log/encrypted-go-backup/
-sudo install -o $USER_ID -g $USER_ID -m 0774 -p -t /etc/encrypted-go-backup/ install/*
+sudo install -d -o $USER_ID -g $USER_ID -m 0774 -p /etc/encrypted-go-backup/ 
+
+cp install/* /etc/encrypted-go-backup/
 
 sudo cp system/* /etc/systemd/system/
