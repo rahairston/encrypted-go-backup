742d099ac95a2320d1c8cf5487c408c23d243450
rahairston <rahairston2014@verizon.net>
2023-08-28 11:54:14 -0500

    updating timer to be a template for USER call

--- setup.sh
+++ setup.sh
@@ -10,7 +10,11 @@ sudo install -d -o $USER_ID -g $USER_ID -m 0774 -p /etc/encrypted-go-backup/
 
 cp install/* /etc/encrypted-go-backup/
 
-sudo cp system/* /etc/systemd/system/
+sed "s/%i/$USER/" system/timer.template > system/encrypted-go-backup.timer
+
+sudo cp system/encrypted-go-backup* /etc/systemd/system/
+
+rm system/encrypted-go-backup.timer
 
 systemctl start encrypted-go-backup@$USER_ID.service
 systemctl enable encrypted-go-backup.timer
